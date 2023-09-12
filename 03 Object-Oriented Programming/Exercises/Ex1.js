fa4b728eb6787635e871c4d8724e792affa96b7d
blaizans <99187215+blaizans@users.noreply.github.com>
2023-09-12 20:43:49 +0200

    Class3Ex1 done

---
+++ 03 Object-Oriented Programming/Exercises/Ex1.js
@@ -0,0 +1,25 @@
+// Your code here.
+function createCircle(center, radius) {
+    return {
+        getCenterX: () => center.getX(),
+        getCenterY: () => center.getY(),
+        getRadius: () => radius,
+        moveCenterTo: (x, y) => center.moveTo(x, y),
+        toString: () => `Circle(Center: ${center.toString()}, Radius: ${radius})`,
+    }
+}
+
+function createPoint(x, y) {
+    return {
+        getX: () => x,
+        getY: () => y,
+        moveTo(newX, newY) {
+            x = newX
+            y = newY
+        },
+        toString: () => `[${x},${y}]`,
+    }
+}
+
+console.log(createCircle(createPoint(1, 2), 4).toString());
+// → Circle(Center: [1,2], Radius: 4)
